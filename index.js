12fc54a70bf70542b611aaa529b2bec60619d6c2
Martin Achtermann <65299501+roadpilot@users.noreply.github.com>
2021-10-15 01:54:42 +0000

    algo club solution

--- index.js
+++ index.js
@@ -1,5 +1,22 @@
 /*
 3. Longest Substring Without Repeating Characters
 
-Given a string s, find the length of the longest substring without repeating characters.
+Constraints:
+0 <= s.length <= 5 * 104
+s consists of English letters, digits, symbols and spaces.
+
 */
+
+const lengthOfLongestSubstring = function(s) {
+  let currSub = [];
+  let longSubLength = 0;
+  for (let i = 0; i < s.length; i++) {
+    const charIdx = currSub.indexOf(s[i])
+    if (charIdx !== -1) {
+      currSub.splice(0, charIdx + 1)
+    }
+    currSub.push(s[i])
+    longSubLength = Math.max(longSubLength, currSub.length)
+  }
+  return longSubLength
+}
